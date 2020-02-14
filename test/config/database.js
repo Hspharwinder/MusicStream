2bb46d80de3a717ee2501ef7eeaa39f694937a0d
HSP <40883035+Hspharwinder@users.noreply.github.com>
2020-02-14 16:47:44 +0530

    Update database.js

--- test/config/database.js
+++ test/config/database.js
@@ -1,12 +1,12 @@
 // This is an example database config file. Replace these presets with your database info and add this file to your .gitignore.
 module.exports = {
   'connection': {
-    'host': '185.149.112.29',
-    'user': 'shyammusic',
-    'password': 'shyammusic',
+    'host': '185.149.112.50',
+    'user': 'username',
+    'password': '******',
     'multipleStatements': 'true'
   },
-  'database': 'ShyamMusicDB'
+  'database': 'dataabseName'
 };
 
 
